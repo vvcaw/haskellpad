e97de1748c90848e5901e0626930af1f4dc5f64c
vvcaw <eric.elflein@googlemail.com>
2023-03-09 16:56:03 +0100

    Add example for changing widget list

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -4,6 +4,8 @@
 module Frontend where
 
 import Control.Monad
+import Control.Monad.Fix
+import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Language.Javascript.JSaddle (eval, liftJSM)
@@ -32,23 +34,64 @@ frontend =
               "type" =: "text/css" <> "rel" =: "stylesheet")
              blank
     , _frontend_body =
-        do el "h1" $ text "λ Welcome to Obelisk!"
-           el "p" $ text $ T.pack commonStuff
-           el "div" $ do
-             t <- inputElement def
-             text " "
-             divClass "bg-red-700 font-fira" $ dynText $ _inputElement_value t
-      -- `prerender` and `prerender_` let you choose a widget to run on the server
-      -- during prerendering and a different widget to run on the client with
-      -- JavaScript. The following will generate a `blank` widget on the server and
-      -- print "Hello, World!" on the client.
-           prerender_ blank $
-             liftJSM $ void $ eval ("console.log('Hello, World!')" :: T.Text)
-           elAttr "img" ("src" =: $(static "obelisk.jpg")) blank
-           el "div" $ do
-             exampleConfig <- getConfig "common/example"
-             case exampleConfig of
-               Nothing -> text "No config file found in config/common/example"
-               Just s -> text $ T.decodeUtf8 s
-           return ()
+        do divClass "bg-WHITE font-fira w-full h-screen" $ do
+             add <- button "Add"
+             rem <- button "Remove"
+             listLen
+               -- | Fold over, every time a event triggeres
+                <-
+               foldDyn
+                 (\x ->
+                    case x of
+                      Add n -> \acc -> Add (currentId acc + n)
+                      Remove n -> \acc -> Remove (currentId acc - n))
+                 (Add 0) $
+               -- | Fires, if one of the events gets executed if multiple, chooses the left one
+               leftmost [Add 1 <$ add, Remove 1 <$ rem]
+             divClass "bg-red-700" $ dynText (T.pack . show <$> listLen)
+             el "div" $ text "λ Welcome to Obelisk!"
+             el "p" $ text $ T.pack commonStuff
+             el "div" $ do
+               t <- inputElement def
+               text " "
+               divClass "bg-red-700 font-fira" $ dynText $ _inputElement_value t
+             el "div" $
+               void $
+               -- | basically literally makes a list with changing widgets based on the given Event
+               listHoldWithKey
+                 mempty -- Monoidic identity
+                 -- | `updated` basically reevalutes this whenever the Dynamic listLen changes
+                 (toMap <$> updated listLen)
+                 (\_ _ -> counter)
     }
+
+buildConsole :: DomBuilder t m => m ()
+buildConsole = divClass "w-full h-40 border border-PURPLE" $ return ()
+
+data AddRemove
+  = Add Int
+  | Remove Int
+  deriving (Show)
+
+currentId :: AddRemove -> Int
+currentId x =
+  case x of
+    Add n -> n
+    Remove n -> n
+
+counter ::
+     Control.Monad.Fix.MonadFix m
+  => (DomBuilder t m, PostBuild t m, MonadHold t m) =>
+       m ()
+counter = do
+  down <- button "<"
+  up <- button ">"
+  c <- foldDyn id (0 :: Int) $ leftmost [(+ 1) <$ up, (subtract 1) <$ down]
+  dynText (T.pack . show <$> c)
+
+-- Just x means, the widget should render, Nothing means it shouldn't
+toMap :: AddRemove -> M.Map Int (Maybe ())
+toMap x =
+  case x of
+    Add n -> M.singleton n (Just ())
+    Remove n -> M.singleton (n + 1) Nothing
